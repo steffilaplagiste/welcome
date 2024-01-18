e4acf1345e475a6cbd56b2b4611a9c64cd43a33a
Steffi Marie <steffilaplagiste@users.noreply.github.com>
2024-01-18 09:06:28 +0100

    Add files via upload

---
+++ exercice2.js
@@ -0,0 +1,10 @@
+function abracadabra() {
+    let age = prompt('entrez votre age');
+    let nom = prompt('entrez votre nom');
+    let prenom = prompt('entrez votre prenom');
+
+    alert("Sapristi ! On ne m'avait pas prévenu que c'était vous, " + prenom + " ! Euh... Je veux dire... Monsieur le grand magicien " + nom + " ! Cela fait déjà " + age + " ans que vous faites rayonner notre contrée !");
+    
+}
+
+abracadabra();
